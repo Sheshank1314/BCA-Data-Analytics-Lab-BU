043d6e36fe2b729c22f75d40599e41bf11318394
Sheshank Bandarupalli <151662017+Sheshank1314@users.noreply.github.com>
2025-12-05 22:18:03 +0530

    Add files via upload

---
+++ Probability.py
@@ -0,0 +1,6 @@
+# Simple probability
+# Probability of rolling a 4 on a six-sided die
+total_outcomes = 6
+favorable_outcomes = 1 # Rolling a 4
+probability_4 = favorable_outcomes / total_outcomes
+print("Probability of rolling a 4:", probability_4)
